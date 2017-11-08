302d51769bd226810e172666d69347f16aa46866
bendixsonnenberg <bendix.sonnenberg@gmx.de>
2017-11-08 21:48:47 +0100

    created basic java file

---
+++ NodeTree.java
@@ -0,0 +1,12 @@
+class NodeTree {
+    
+}
+class Node{
+    /*
+     * This class models a node in the tree that has to be created
+     * we could add some extra types for example operands for numbers, but
+     * for now i won't bother
+     * */
+    enum operand = {BITWISE_AND,BITWISE_XOR,BITWISE_OR,NOT,AND,OR};
+    
+}
