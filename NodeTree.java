97df93795b5a8d9dc2e0374ed7eee5489fa4eb64
bendixsonnenberg <bendix.sonnenberg@gmx.de>
2017-11-08 22:05:38 +0100

    constructor for node

--- NodeTree.java
+++ NodeTree.java
@@ -1,5 +1,7 @@
 class NodeTree {
-    
+   public static void main(String args[]){
+        Node a = new Node(null,null,null);
+   } 
 }
 class Node{
     /*
@@ -7,6 +9,15 @@ class Node{
      * we could add some extra types for example operands for numbers, but
      * for now i won't bother
      * */
-    enum operand = {BITWISE_AND,BITWISE_XOR,BITWISE_OR,NOT,AND,OR};
+    static enum Operand {PLACEHOLDER,BITWISE_AND,BITWISE_XOR,BITWISE_OR,NOT,AND,OR};
+    Operand type;
+    Node input1,input2;
+    String name;
+    public Node(Operand nodeOp, Node input1, Node input2){
+        type = nodeOp;
+        this.input1 = input1;
+        this.input2 = input2;
+        if(type == Operand.PLACEHOLDER) name = "TODO";
+    }
     
 }
